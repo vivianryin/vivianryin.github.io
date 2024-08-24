b956063b9885398fee4e9942c78d1811517e5647
Vivian Yin <vivian.yin@columbia.edu>
2024-08-24 02:07:26 -0400

    updating ui

---
+++ assets/js/text_animation.js
@@ -0,0 +1,19 @@
+	function tm_animate_text(){
+	
+		"use strict";
+		
+		var animateSpan			= jQuery('.animation_text_word');
+		
+			animateSpan.typed({
+				strings: ["Data Analyst", "Data Scientist", "Business Analyst", "Columbia Student"],
+				loop: true,
+				startDelay: 0,
+				backDelay: 0
+			});
+	}
+
+	jQuery(document).on('ready', function () {
+		(function ($) {
+			tm_animate_text();
+		})(jQuery);
+	});
